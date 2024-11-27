ae519669bb32bf8f970dc8aa21b98158e1be9293
Khuzaima Yunus <118909350+khuziyunus@users.noreply.github.com>
2024-11-27 23:37:22 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,11 +10,11 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.document_loaders import PyPDFLoader
 
 # Set OpenAI API key securely using Streamlit secrets
-os.environ["OPENAI_API_KEY"] = st.secrets[OPENAI_API_KEY]
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 
 # Set Hugging Face token securely using Streamlit secrets
 from huggingface_hub import HfFolder
-HfFolder.save_token(st.secrets[HF_API_TOKEN])
+HfFolder.save_token(st.secrets["HF_API_TOKEN"])
 
 # Load the PDF and preprocess
 loader = PyPDFLoader("chat_bot_sponser_data.pdf")  # Replace with correct path to PDF
